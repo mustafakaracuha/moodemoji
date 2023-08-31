40173463f847ed5322a0748f4ba148a70624ef17
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-09-01 02:16:02 +0300

    update

--- src/components/Button/EmojiButton/EmojiButton.jsx
+++ src/components/Button/EmojiButton/EmojiButton.jsx
@@ -10,7 +10,7 @@ const EmojiButton = ({ emoji, onClick }) => {
       className={
         selectedEmoji === emoji
           ? "w-10 h-10 flex items-center justify-center text-center text-[42px] pt-1 animate-fade-in outline-none transition duration-500 ring-4 ring-white rounded-full"
-          : "w-10 h-10 flex items-center justify-center text-center text-[42px] pt-1 animate-fade-in outline-none ring-4 ring-transparent rounded-full"
+          : "w-10 h-10 flex items-center justify-center text-center text-[42px] pt-1 animate-fade-in outline-none transition duration-500 ring-4 hover:ring-white ring-transparent rounded-full"
       }
     >
       {emoji}
